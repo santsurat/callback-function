6a7fa530363210509dbb5c80a73adafe0f9c03d4
@SantSurat <santsurat123@gmail.com>
2023-04-27 09:14:35 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,95 @@
+
+// function x(y) {
+//     console.log("x");
+//     y();
+// }
+
+
+// x(function y() {
+//     console.log("y");
+// })
+
+
+
+// setTimeout(  xyz = () => {
+//     console.log("Clicked")
+// }, 2000);
+
+
+// //const add = (x,y) => console.log(x);
+
+
+
+//  const  add = (y) => {
+//  console.log("p");
+//  y(2);
+//  };
+
+// add((y) => {
+//     console.log(y)
+// });
+
+// function attach() {
+//     let count = 0 ;
+// document.getElementById("Clickme").addEventListener("click" ,   () => {
+
+// console.log("Cliked" , ++count);
+
+// })
+// }
+
+// let count = 0 ;
+// document.getElementById("Clickme").addEventListener("click" ,   () => {
+
+// console.log("Cliked" , ++count);
+
+// })
+
+// attach();
+
+
+console.log("start");
+
+document.getElementById("btn")
+.addEventListener("click" , function cb() {
+    console.log("callbck");
+});
+
+
+setTimeout(function cbt() {
+    console.log("cb setTimeout");
+}, 5000);
+
+// fetch("https://api.netflix.com").then(function cbf(){
+//     console.log("cb netflix");
+// })
+
+
+console.log("End");
+
+
+
+
+let startDate = new Date().getTime();;
+let endDate = startDate;
+while (endDate < startDate + 10000) {
+    endDate = new Date().getTime();
+}
+
+console.log("while expires");
+
+
+// const radius = [4 , 6 , 8 , 10] ;
+
+// const area = function (radius) {
+//    return Math.PI * radius * radius ;
+// };
+
+
+// const Calculate = function (radius , logic ){
+//     const output = [];
+
+//     for(let i=0; i<radius.lenght)
+// }
+
+
